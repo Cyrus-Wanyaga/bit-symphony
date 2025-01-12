ec0eb6de21d2574f8448164f9b54748fdcec8b04
Cyrus-Wanyaga <cwanyaga95@gmail.com>
2025-01-13 00:31:10 +0300

    web page improvements

--- src/main/resources/site/templates-dev/index.js
+++ src/main/resources/site/templates-dev/index.js
@@ -3,7 +3,13 @@ const observeWSChanges = async () => {
 
     ws.addEventListener("open", (event) => {
         console.log("Connected to webserver");
-    })
+    });
+    
+    ws.addEventListener("message", (event) => {
+        if (event.data === "reload") {
+            window.location.reload();
+        }
+    });
 };
 
 observeWSChanges();
