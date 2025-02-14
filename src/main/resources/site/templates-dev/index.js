b93015ae9f40ec8d1d8efa701175ed39c75318b1
Cyrus-Wanyaga <cwanyaga95@gmail.com>
2025-02-14 16:51:05 +0300

    add sidebar

--- src/main/resources/site/templates-dev/index.js
+++ src/main/resources/site/templates-dev/index.js
@@ -4,7 +4,7 @@ const observeWSChanges = async () => {
     ws.addEventListener("open", (event) => {
         console.log("Connected to webserver");
     });
-    
+
     ws.addEventListener("message", (event) => {
         if (event.data === "reload") {
             window.location.reload();
@@ -13,3 +13,16 @@ const observeWSChanges = async () => {
 };
 
 observeWSChanges();
+
+// document.querySelectorAll('script[type="text/css"]').forEach(async (script) => {
+//     let cssPath = script.textContent.trim();
+//     let response = await fetch(cssPath);
+//     let cssText = await response.text();
+//     let style = document.createElement("style");
+//     style.innerHTML = cssText;
+//     document.head.appendChild(style);
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     document.body.classList.add("styles-loaded");
+// });
